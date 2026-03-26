b90ad5af23c4a0c2919fbb28a9e105646cc0e67c
Wayne Gakuo <waynejotham@gmail.com>
2026-03-27 01:36:23 +0300

    chore(comments): move TODO comment in `ai.service.ts`

--- src/app/services/ai/ai.service.ts
+++ src/app/services/ai/ai.service.ts
@@ -1,9 +1,10 @@
+// TODO: Define the AI service and its methods
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AiService {
-  // TODO: Define the AI service and its methods
 
 }
